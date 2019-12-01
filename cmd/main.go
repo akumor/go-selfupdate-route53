38dda8c43682d07bbfd196f0accc799979593e1e
akumor <akumor@gmail.com>
2019-11-30 17:29:17 -0800

    Successfully determined public IP.

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,13 @@ package main
 
 import (
 	"log"
-	"net"
-	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/route53"
 	"github.com/aws/aws-sdk-go/service/route53/route53iface"
+
+	externalip "github.com/akumor/go-external-ip"
 )
 
 func main() {
@@ -17,9 +17,9 @@ func main() {
 	//start := time.Now()
 
 	// Define host port id outputfile flags
-	/*apiHost := flag.String("host", "localhost", "API host to query for the wishlist.")
-	apiPort := flag.String("port", "80", "API port to query for the wishlist.")
-	id := flag.String("id", "DEFAULT", "ID of the Amazon wishlist.")
+	/*hostedZoneID := flag.String("hosted-zone-id", "localhost", "AWS Route 53 Hosted Zone ID.")
+	recordName := flag.String("record-name", "80", "DNS record name to upsert.")
+	ttl := flag.String("ttl", "300", "TTL of the DNS record.")
 
 	flag.Parse()
 	*/
@@ -37,36 +37,66 @@ func main() {
 			sess = session.Must(session.NewSession(&aws.Config{
 				Region: aws.String(region)},
 			))
-		}*/
+		}
+		// Create a Route53 client from just a session.
+		r53 := route53.New(sess)
+	*/
 
 	//oldIP := ""
+	// Create the default consensus,
+	// using the default configuration and no logger.
+	consensus := externalip.DefaultConsensus(nil, nil)
 	for {
-
-		// Attempt to get the public IP
-		publicIP, err := getPublicIP()
+		// Get the public IP,
+		// which is never <nil> when err is <nil>.
+		publicIP, err := consensus.ExternalIP()
 		if err != nil {
 			// TODO: allow sleep duration to be configureable
 			log.Printf("getting public IP failed. will retry in %d seconds\n", 300)
 			time.Sleep(time.Duration(300) * time.Second)
 			continue
 		}
-		log.Printf("Obtained public IP: %s\n", publicIP)
+		log.Printf("Obtained public IP: %s\n", publicIP.String()) // print IPv4/IPv6 in string format
+
+		publicIPstr := publicIP.String()
+		isIPv4 := false
+		for i := 0; i < len(publicIPstr); i++ {
+			switch publicIPstr[i] {
+			case '.':
+				isIPv4 = true
+				break
+			case ':':
+				isIPv4 = false
+				break
+			}
+		}
+
+		if isIPv4 {
+			log.Println("detected IPv4 IP address")
+		} else {
+			log.Println("detected IPv6 IP address")
+		}
 
 		/*
-			if oldIP != publicIP {
-				// Create a Route53 client from just a session.
-				r53 := route53.New(sess)
-				createCNAME(r53)
-				if err != nil {
-					log.Println("failed to update AWS Route 53. will retry in %d seconds", 300)
-					time.Sleep(300)
-					continue
+				if oldIP != publicIP {
+					if isIPv4 {
+			        	log.Println("detected IPv4 IP address")
+			        	recordType := "A"
+			        } else {
+			        	log.Println("detected IPv6 IP address")
+			        	recordType := "AAAA"
+			        }
+					createRecord(r53, recordType, hostedZoneID, recordName, publicIPstr, ttl)
+					if err != nil {
+						log.Println("failed to update AWS Route 53. will retry in %d seconds", 300)
+						time.Sleep(300)
+						continue
+					}
+					log.Println("Route53 record updated")
+					oldIP = publicIP
+				} else {
+					log.Println("Public IP did not change. Nothing to do.")
 				}
-				log.Println("Route53 record updated")
-				oldIP = publicIP
-			} else {
-				log.Println("Public IP did not change. Nothing to do.")
-			}
 		*/
 
 		// TODO: Allow sleep duration to be configurable
@@ -76,15 +106,16 @@ func main() {
 	//log.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
 
-func createCNAME(svc route53iface.Route53API, zoneID, name, target string, ttl int64) error {
+// createRecord performs an UPSERT of a DNS record AWS Route 53
+func createRecord(svc route53iface.Route53API, recordType, zoneID, name, target string, ttl int64) error {
 	params := &route53.ChangeResourceRecordSetsInput{
 		ChangeBatch: &route53.ChangeBatch{ // Required
 			Changes: []*route53.Change{ // Required
 				{ // Required
 					Action: aws.String("UPSERT"), // Required
 					ResourceRecordSet: &route53.ResourceRecordSet{ // Required
-						Name: aws.String(name),    // Required
-						Type: aws.String("CNAME"), // Required
+						Name: aws.String(name),       // Required
+						Type: aws.String(recordType), // Required
 						ResourceRecords: []*route53.ResourceRecord{
 							{ // Required
 								Value: aws.String(target), // Required
@@ -105,42 +136,4 @@ func createCNAME(svc route53iface.Route53API, zoneID, name, target string, ttl i
 	}
 	// TODO: wait for Route53 DNS servers to become in sync: https://github.com/aws/aws-sdk-go/blob/v1.25.42/service/route53/api.go#L2489
 	return nil
-}
-
-// getPublicIP returns the public ip address
-func getPublicIP() (string, error) {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		return "", err
-	}
-	defer conn.Close()
-	localAddr := conn.LocalAddr().String()
-	idx := strings.LastIndex(localAddr, ":")
-	return localAddr[0:idx], nil
-}
-
-// isPublicIP returns true if provided IP is a valid public IP and false otherwise
-func isPublicIP(IP net.IP) bool {
-	//Class       Starting IPAddress     Ending IP Address    # of Hosts
-	//A           10.0.0.0               10.255.255.255       16,777,216
-	//B           172.16.0.0             172.31.255.255       1,048,576
-	//C           192.168.0.0            192.168.255.255      65,536
-	//Link-local  169.254.0.0            169.254.255.255      65,536
-	//Local       127.0.0.0              127.255.255.255      16777216
-	if IP.IsLoopback() || IP.IsLinkLocalMulticast() || IP.IsLinkLocalUnicast() {
-		return false
-	}
-	if ip4 := IP.To4(); ip4 != nil {
-		switch {
-		case ip4[0] == 10:
-			return false
-		case ip4[0] == 172 && ip4[1] >= 16 && ip4[1] <= 31:
-			return false
-		case ip4[0] == 192 && ip4[1] == 168:
-			return false
-		default:
-			return true
-		}
-	}
-	return false
 }
